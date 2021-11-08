6329efd647e92f86611f3dd34366f08f2084cacd
ailict <ailichatfieldtaylor@gmail.com>
2021-11-07 20:56:15 -0500

    added starter code from TA Justin

---
+++ index.js
@@ -0,0 +1,11 @@
+## Top of index.js
+const render = require("./src/page-template.js");
+
+## After you have built the teams and stored them in a global array
+function buildTeam() {
+    // Create the output directory if the output path doesn't exist
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  }
